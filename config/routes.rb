834843489e3b02d8699b010f142c53120ebe0646
Luis Zarza <luis@luiszarza.com>
2023-02-27 17:47:23 +0800

    created routes, and action for booking create

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :listings
+  resources :listings do
+    resources :bookings, only: :create
+  end
 end
