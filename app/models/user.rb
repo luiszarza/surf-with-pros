198cc1f4f31155fc793396ffba6f194f7b727517
Aidan Keenan <aidankeenan7137@gmail.com>
2023-03-02 14:39:35 +0800

    worked on the bookings page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings
-  has_many :listings
+  has_many :bookings # as student
+  has_many :listings # as teacher
   has_many :bookings_as_teacher, through: :listings, source: :bookings
 end
